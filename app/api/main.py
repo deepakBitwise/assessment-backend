f325ce374cb03fb67913c261cc02e19bd4a3c738
mayankraj179 <Mayank.Raj@bitwiseglobal.com>
2026-05-19 16:56:09 +0530

    Added hyman-review logic

--- app/api/main.py
+++ app/api/main.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from app.api.routes import assessment, files, items, login, private, submission, users, utils
 from app.core.config import settings
+from app.api.routes import human_review
 
 api_router = APIRouter()
 api_router.include_router(login.router)
@@ -11,6 +12,7 @@ api_router.include_router(items.router)
 api_router.include_router(assessment.router)
 api_router.include_router(files.router)
 api_router.include_router(submission.router)
+api_router.include_router(human_review.router)
 
 
 if settings.ENVIRONMENT == "local":
